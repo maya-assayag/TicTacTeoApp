1e14e2ff1c27daff5aa7fa1a67852984937d9f68
Maya Assayag <maya.assyag@gmail.com>
2021-11-13 19:17:00 +0200

    add Game display view.

---
+++ app/src/main/java/com/example/tictactoe/GameDisplay.java
@@ -0,0 +1,26 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class GameDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_display);
+    }
+
+    public void playAgainButtonClick(View view){
+
+    }
+
+    public void homeButtonClick(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
